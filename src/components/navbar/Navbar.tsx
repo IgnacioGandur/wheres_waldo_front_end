e9c87694830c92d6a98ce8d2fd5e300e6de089a7
Ignacio Gandur <gandurjuanignacio@gmail.com>
2025-08-10 02:17:05 -0300

    Progress: Simplified the SelectGame screen design.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -35,6 +35,8 @@ const Navbar = ({ toggleVisibility }: NavbarTypes) => {
         className={styles.navbar}
     >
         <div className={styles.logo}>
+            <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none"><path fill="#fff" fill-opacity="0.25" fill-rule="evenodd" d="M12 19a7 7 0 1 0 0-14a7 7 0 0 0 0 14M10.087 7.38A5 5 0 0 1 12 7a.5.5 0 0 0 0-1a6 6 0 0 0-6 6a.5.5 0 0 0 1 0a5 5 0 0 1 3.087-4.62" clip-rule="evenodd" /><path stroke="#fff" stroke-linecap="round" d="M20.5 20.5L17 17" stroke-width="1" /><circle cx="11" cy="11" r="8.5" stroke="#fff" stroke-width="1" /></g></svg>
+            <div className={styles["vertical-separator"]}></div>
             <h1>Find The Character</h1>
         </div>
         <div className={styles.links}>
--- src/pages/select-game/SelectGame.tsx
+++ src/pages/select-game/SelectGame.tsx
@@ -1,42 +1,66 @@
 import styles from "./SelectGame.module.css"
+import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 import { useLoaderData, NavLink } from "react-router";
 
+type GameData = {
+    fail: boolean;
+    error: boolean;
+    message: string;
+    games: {
+        name: string;
+        slug: string;
+        filename: string;
+        link: string;
+        difficulty: string;
+        fail: boolean;
+        error: string;
+        data: JSON;
+    }[]
+};
+
 const SelectGame = () => {
-    const loaderData = useLoaderData();
+    const loaderData: GameData = useLoaderData();
+
     return <main className={styles["select-game"]}>
-        <header className={styles["header"]}>
-            {loaderData.fail && <p className={styles.error}>
-                {loaderData.error}
-            </p>}
-            <h1>
-                Which game would you like to play?
-            </h1>
-        </header>
-        <div className={styles["games-preview"]}>
-            {loaderData.games.map((game: any) => {
-                return <NavLink
-                    viewTransition
-                    key={game.name}
-                    className={styles["game"]}
-                    to={`/games/${game.slug}`}
-                >
-                    <img
-                        className={styles["background-image"]}
-                        src={`/images/previews/${game.filename}`.split(".")[0] + ".jpg"}
-                        alt={game.name}
-                    />
-                    <div className={styles["overlay"]}>
-                        <h2
-                            className={styles["title"]}
-                        >
-                            {game.name}
-                        </h2>
-                        <p className={styles[`difficulty`]}>
-                            {game.difficulty}
-                        </p>
-                    </div>
-                </NavLink>
-            })}
+        <div className={styles["background"]}>
+            <div className={styles["overlay"]}></div>
+        </div>
+        <div className={styles["content"]}>
+            <header className={styles["header"]}>
+                {loaderData.fail && <p className={styles.error}>
+                    {loaderData.error}
+                </p>}
+                <h1>
+                    Which game would you like to play?
+                </h1>
+            </header>
+            <div className={styles["games-preview"]}>
+                {loaderData.games.map((game: any) => {
+                    return <NavLink
+                        viewTransition
+                        key={game.name}
+                        className={styles["game"]}
+                        to={`/games/${game.slug}`}
+                    >
+                        <img
+                            className={styles["background-image"]}
+                            src={`/images/previews/${game.filename}`.split(".")[0] + ".jpg"}
+                            alt={game.name}
+                        />
+                        <div className={styles["overlay"]}>
+                            <h2
+                                className={styles["title"]}
+                            >
+                                {game.name}
+                            </h2>
+                            <p className={styles[`difficulty`]}>
+                                {game.difficulty}
+                            </p>
+                        </div>
+                    </NavLink>
+                })}
+            </div>
         </div>
     </main>
 }
